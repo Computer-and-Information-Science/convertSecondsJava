a793e4c6e52f0862f190556278771ea8d8ef9c05
Lisa Waddell <90416736+techteacher101nj@users.noreply.github.com>
2025-09-06 13:59:46 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,16 @@
+//To DO: your name here
+//To Do: import statements
 
+public class Main {
+  public static void main(String[] args) {
+    //Prompt for input
+
+    //Read Input
+      
+    //convert using division and the remainder operator %
+     
+    //output the conversion
+    
+    
+   }
+}
